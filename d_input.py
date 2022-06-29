8c9ac6aa703ff7c08db6c84b20242809ca37e151
bouaziz <50086211+Bouaziz19@users.noreply.github.com>
2022-06-29 10:08:23 +0000

    v4

---
+++ d_input.py
@@ -0,0 +1,14 @@
+global n_tch 
+# n_tch = 5
+t=0
+while (t==0):
+  try:
+    n_tch = 3
+    # if is input
+    # n_tch = int(input('nt:'))
+    t=1
+  except:
+    print("yalhaj rakem ")
+
+
+list_pt=[10,14,52,41,42]
